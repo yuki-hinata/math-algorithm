52d148ffa11fef3d60363e8f2b5599932087ad5c
yuki-hinata <bigisland20001010@gmail.com>
2022-09-18 10:53:24 +0900

    feat: 環境構築

---
+++ src/Sosuu.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Sosuu() {
+
+    return (
+        <div className="App">
+            <p>っっっｊ</p>
+        </div>
+    );
+}
+
+export default Sosuu;
